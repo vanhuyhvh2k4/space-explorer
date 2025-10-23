6e5d5ad53996cf2e7829ff29dab947daa7334f7d
= <nguyenthitrangyiiii@gmail.com>
2025-10-23 19:18:24 +0700

    init

---
+++ Assets/Script/ParallaxBackground.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+ 
+public class PrallaxBackground : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    float backgroundWidth;
+    void Start()
+    {
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        backgroundWidth = sprite.texture.width / sprite.pixelsPerUnit;
+    }
+
+    void Update()
+    {
+        float moveX = moveSpeed * Time.deltaTime;
+        transform.position += new Vector3(moveX, 0);
+        if (Math.Abs(transform.position.x) - backgroundWidth > 0)
+        {
+            transform.position = new Vector3(0f, transform.position.y);
+        }
+    } 
+}
